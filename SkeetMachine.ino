60bdd4819a8b4d961f21be9c6dc66df35237e064
Magnús Ragnarsson <magnus@ru.is>
2013-03-09 00:25:22 +0000

    VERSION 1.1
    Sama lýsing  og áðan

--- SkeetMachine.ino
+++ SkeetMachine.ino
@@ -5,11 +5,11 @@ const int ch_B = 10; //turn
 const int ch_C = 9;  //double
 const int ch_D = 8;  //Unused
 
-const int ledPin = 0; //prufuljós
+const int ledPin = 13; //prufuljós
 
 //relay pinnar
-const int relay_1 = 13; //Motor á mark
-const int relay_2 = 13; //Motor á turni
+const int relay_1 = 2; //Motor á mark
+const int relay_2 = 3; //Motor á turni
 const int relay_3 = 4; //Ljos á marki
 const int relay_4 = 5; //Ljos á turni
 
@@ -29,8 +29,8 @@ unsigned long s1time= 0;
 unsigned long s2time= 0;
 unsigned long randDelay = 0;
 int mark = 0;
-int sensor1 = 1;
-int sensor2 = 1;
+int sensor1 = HIGH;
+int sensor2 = HIGH;
 
 void setup() {
   
@@ -55,7 +55,13 @@ void setup() {
   pinMode(sensor_1, INPUT); // skynjari á marki
   pinMode(sensor_1, INPUT); // skynjari á turni
   
-  Serial.begin(9600);
+  //Stillir relayin rett þegar forritid er ræst
+  digitalWrite(relay_1, HIGH);
+  digitalWrite(relay_2, HIGH);
+  digitalWrite(relay_3, HIGH);
+  digitalWrite(relay_4, HIGH);
+  
+  Serial.begin(9600); //serial skilgreint a hradanum 9600
   
 }
   
@@ -65,7 +71,9 @@ void loop() {
  buttonStateB = digitalRead(ch_B);
  buttonStateC = digitalRead(ch_C);
  
-   if((buttonStateA != lastButtonStateA) && (sensor1 != 0) && (mark == 0)) {
+ 
+    //Tekur a moti skipun ef breyting verdur, og velin er ekki ad trekkja sig og engin skipun er i gangi fyrir annan kastara
+    if((buttonStateA != lastButtonStateA) && (sensor1 != 0) && (mark == LOW)) {
      if(buttonStateA == HIGH) {
        //ef merki kemur inn á ch1 þá senda mark 1
        Serial.println("Button pushed for Mark");
@@ -74,8 +82,8 @@ void loop() {
      } 
     
    }
-   
-   if((buttonStateB != lastButtonStateB) && (sensor2 != 0) && (mark == 0)) {
+   //sama og fyrir ofan, nema fyrir takka b
+   if((buttonStateB != lastButtonStateB) && (sensor2 != 0) && (mark == LOW)) {
      if(buttonStateB == HIGH) {
        //ef merki kemur inn á ch2 þá senda mark 2
        Serial.println("Button pushed for Tower!");
@@ -83,9 +91,10 @@ void loop() {
      } 
    }
    
-   if((buttonStateC != lastButtonStateC) && ((sensor1 != 0) || (sensor2 != 0)) && (mark == 0)){
+   //sama og fyrir ofan nema fyrir takka c
+   if((buttonStateC != lastButtonStateC) && ((sensor1 != 0) && (sensor2 != 0)) && (mark == LOW)){
      if(buttonStateC == HIGH) {
-       //ef merki kemur inn á ch3 þá senda mark 2
+       //ef merki kemur inn á ch3 þá senda mark 3
        Serial.println("Button pushed for double!");
        mark = 3;
      } 
@@ -97,45 +106,46 @@ void loop() {
        randDelay = random(3000);
        time = millis();
        if(mark == 1) {
-         digitalWrite(relay_3, HIGH);
+         digitalWrite(relay_3, LOW);
        }
        else if (mark == 2) {
-         digitalWrite(relay_4, HIGH);
+         digitalWrite(relay_4, LOW);
        }
        else if (mark == 3) {
-         digitalWrite(relay_3, HIGH);
-         digitalWrite(relay_4, HIGH);
+         digitalWrite(relay_3, LOW);
+         digitalWrite(relay_4, LOW);
        }
-         
+       
+       //skrifar ut a serial hvad random timinn er  
        Serial.print("Delay in ms: ");
        Serial.println(randDelay);
        
      }
      
      //þegar timerinn er liðinn þá slekkur þetta á ljósi og skýtur dúfu af stað
-     if(((time + randDelay) <= millis()) && mark != 0 ) {
+     if(((time + randDelay) <= millis()) && mark != 0 ) {//profar hvort ad random timinn er lidinn og skipun hefur verid gefin
        if(mark == 1){
-         digitalWrite(relay_1, HIGH);
+         digitalWrite(relay_1, LOW);
          Serial.println("CLAY GOES OFF!! mark");
-         digitalWrite(relay_3, LOW);
-         sensor1 = 0;
+         digitalWrite(relay_3, HIGH);
+         sensor1 = LOW;
          s1time = millis();
        }
        else if (mark == 2) {
-         digitalWrite(relay_2, HIGH);
+         digitalWrite(relay_2, LOW);
          Serial.println("CLAY GOES OFF!! tower");
-         digitalWrite(relay_4, LOW);
-         sensor2 = 0;
+         digitalWrite(relay_4, HIGH);
+         sensor2 = LOW;
          s2time = millis();
        }
        else if (mark == 3) {
-         digitalWrite(relay_1, HIGH);
+         digitalWrite(relay_1, LOW);
          Serial.println("CLAY GOES OFF!! mark");
-         digitalWrite(relay_2, HIGH);
+         digitalWrite(relay_2, LOW);
          Serial.println("CLAY GOES OFF!! tower");
-         digitalWrite(relay_3, LOW);
-         digitalWrite(relay_4, LOW);
-         sensor1 = sensor2 = 0;
+         digitalWrite(relay_3, HIGH);
+         digitalWrite(relay_4, HIGH);
+         sensor1 = sensor2 = LOW;
          s1time = s2time = millis();
        }
 
@@ -150,17 +160,17 @@ void loop() {
    
    if ( ((s1time + 4000) < millis()) && (sensor1 == 0)) {
      Serial.println("Mark ready again!");
-     digitalWrite(relay_1, LOW);
-     sensor1 = 1;
+     digitalWrite(relay_1, HIGH);
+     sensor1 = HIGH;
    }
    
    if ( ((s2time + 4000) < millis()) && (sensor2 == 0)) {
      Serial.println("Tower ready again!");
-     digitalWrite(relay_2, LOW);
-     sensor2 = 1;
+     digitalWrite(relay_2, HIGH);
+     sensor2 = HIGH;
    }
      
-   
+
    lastButtonStateA = buttonStateA;
    lastButtonStateB = buttonStateB;
    lastButtonStateC = buttonStateC;
